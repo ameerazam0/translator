7aac8cf44f92d363c427fb1d92e629af762f777c
ameerazam0 <72248681+ameerazam0@users.noreply.github.com>
2021-01-21 01:22:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import datetime as dt
 from googletrans import Translator
-import gtts
-from playsound import playsound
-import os
+'''import gtts'''
+'''from playsound import playsound'''
+'''import os
+import pyttsx3
+engine = pyttsx3.init()'''
+
+
 trans=Translator()
 st.title('First streamlit project')
 now=dt.datetime.now()
@@ -11,11 +15,12 @@ now=dt.datetime.now()
 st.write(f"it is now {now}")
 
 input_text=st.text_input('Enter English  Word')
-if st.button('Listen input'):
-    tts = gtts.gTTS(input_text, lang="en")
-    tts.save("hola.mp3")
-    playsound("hola.mp3")
-    os.remove("hola.mp3")
+'''if st.button('Listen input'):
+    #tts = gtts.gTTS(input_text, lang="en")
+    engine.say(input_text)
+    engine.runAndWait()
+    #playsound("hola.mp3")
+    #os.remove("hola.mp3")'''
 
 
 
@@ -31,11 +36,12 @@ dict={'arabic': 'ar','bengali': 'bn','english': 'en','french': 'fr','gujarati': 
     }
 option = st.selectbox( 'Choose Language in this menu',('arabic','bengali','french','gujarati','hindi','indonesian','japanese','malayalam','marathi','myanmar','nepali','punjabi','sindhi','tamil','telugu'))
 result=trans.translate(input_text,dest=dict[option]).text
-if st.button('Listen output'):
+'''if st.button('Listen output'):
     tts = gtts.gTTS(result, lang=dict[option])
     tts.save("hola.mp3")
     playsound("hola.mp3")
-    os.remove("hola.mp3")
+    os.remove("hola.mp3")'''
+
 if st.button('Translate'):
     st.success(result)
     st.write(type(dict[option]))
